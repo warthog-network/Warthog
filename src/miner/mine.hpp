2153b558a751dae8c4a6cc12122fab7fb8288fb2
Pumbaa, Timon & Rafiki <130585176+ByPumbaa@users.noreply.github.com>
2023-10-08 10:32:27 +0000

    delete unused files

--- src/miner/mine.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstddef>
-#include <cstdint>
-#include <tuple>
-#include "block/header/header.hpp"
-
-std::tuple<bool, bool, uint32_t> mine(Header& header, uint32_t stop, uint32_t tries);
