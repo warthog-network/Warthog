warthog-network/Warthog

commit 974fabb078ded25c279ed028a796896621d3701e

fix bug where incremental updates are not sent before peer's init message was received

src/node/eventloop/eventloop.cpp
@@ -228,7 +228,7 @@ void Eventloop::update_chain(Append&& m)
 {
     const auto msg = chains.update_consensus(std::move(m));
     log_chain_length();
-    for (auto c : connections.initialized()) {
+    for (auto c : connections.all()) {
         try {
             c->chain.on_consensus_append(chains);
         } catch (ChainError e) {
@@ -248,7 +248,7 @@ void Eventloop::update_chain(Fork&& fork)
 {
     const auto msg { chains.update_consensus(std::move(fork)) };
     log_chain_length();
-    for (auto c : connections.initialized()) {
+    for (auto c : connections.all()) {
         try {
             c->chain.on_consensus_fork(msg.forkHeight, chains);
             c.send(msg);
@@ -267,7 +267,7 @@ void Eventloop::update_chain(RollbackData&& rd)
     const auto msg { chains.update_consensus(rd) };
     if (msg) {
         log_chain_length();
-        for (auto c : connections.initialized()) {
+        for (auto c : connections.all()) {
             c->chain.on_consensus_shrink(chains);
             c.send(*msg);
         }

commit 9e511f9297128b1983a56e144c284b82b4b8a362

v0.4.30: fix bug introduced in 974fabb0

meson.build
@@ -1,5 +1,5 @@
 project( 'Warthog', ['c','cpp'],
-  version : '0.4.29',
+  version : '0.4.30',
   default_options : ['warning_level=3', 'cpp_std=c++20'])
 
 libuv_dep = subproject('libuv', default_options : ['warning_level=0', 'werror=false', 'build_tests=false']).get_variable('libuv_dep')

src/node/eventloop/eventloop.cpp
@@ -230,7 +230,8 @@ void Eventloop::update_chain(Append&& m)
     log_chain_length();
     for (auto c : connections.all()) {
         try {
-            c->chain.on_consensus_append(chains);
+            if (c.initialized()) 
+                c->chain.on_consensus_append(chains);
         } catch (ChainError e) {
             close(c, e);
         }
@@ -250,7 +251,8 @@ void Eventloop::update_chain(Fork&& fork)
     log_chain_length();
     for (auto c : connections.all()) {
         try {
-            c->chain.on_consensus_fork(msg.forkHeight, chains);
+            if (c.initialized()) 
+                c->chain.on_consensus_fork(msg.forkHeight, chains);
             c.send(msg);
         } catch (ChainError e) {
             close(c, e);
@@ -268,7 +270,8 @@ void Eventloop::update_chain(RollbackData&& rd)
     if (msg) {
         log_chain_length();
         for (auto c : connections.all()) {
-            c->chain.on_consensus_shrink(chains);
+            if (c.initialized()) 
+                c->chain.on_consensus_shrink(chains);
             c.send(*msg);
         }
     }