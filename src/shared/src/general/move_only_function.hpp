04cc0667e86875c63387e2411cdea242eee874fa
CoinFu Master Shifu <145641460+CoinFuMasterShifu@users.noreply.github.com>
2025-10-30 17:25:50 +0100

    switch to C++23

--- src/shared/src/general/move_only_function.hpp
+++ src/shared/src/general/move_only_function.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#if __cplusplus >= 202302L
+#include <functional>
+template <typename T>
+using MoveOnlyFunction = std::move_only_function<T>;
+#else
 /*
 MIT License
 
@@ -84,223 +89,223 @@ namespace ofats {
 
 namespace any_detail {
 
-    using buffer = std::aligned_storage_t<sizeof(void*) * 2, alignof(void*)>;
+using buffer = std::aligned_storage_t<sizeof(void*) * 2, alignof(void*)>;
 
-    template <class T>
-    inline constexpr bool is_small_object_v = sizeof(T) <= sizeof(buffer) && alignof(buffer) % alignof(T) == 0 && std::is_nothrow_move_constructible_v<T>;
+template <class T>
+inline constexpr bool is_small_object_v = sizeof(T) <= sizeof(buffer) && alignof(buffer) % alignof(T) == 0 && std::is_nothrow_move_constructible_v<T>;
 
-    union storage {
-        void* ptr_ = nullptr;
-        buffer buf_;
-    };
+union storage {
+    void* ptr_ = nullptr;
+    buffer buf_;
+};
 
-    enum class action { destroy,
-        move };
+enum class action { destroy,
+    move };
 
-    template <class R, class... ArgTypes>
-    struct handler_traits {
-        template <class Derived>
-        struct handler_base {
-            static void handle(action act, storage* current, storage* other = nullptr)
-            {
-                switch (act) {
-                case (action::destroy):
-                    Derived::destroy(*current);
-                    break;
-                case (action::move):
-                    Derived::move(*current, *other);
-                    break;
-                }
-            }
-        };
-
-        template <class T>
-        struct small_handler : handler_base<small_handler<T>> {
-            template <class... Args>
-            static void create(storage& s, Args&&... args)
-            {
-                new (static_cast<void*>(&s.buf_)) T(std::forward<Args>(args)...);
-            }
-
-            static void destroy(storage& s) noexcept
-            {
-                T& value = *static_cast<T*>(static_cast<void*>(&s.buf_));
-                value.~T();
-            }
-
-            static void move(storage& dst, storage& src) noexcept
-            {
-                create(dst, std::move(*static_cast<T*>(static_cast<void*>(&src.buf_))));
-                destroy(src);
-            }
-
-            static R call(storage& s, ArgTypes... args)
-            {
-                return std::invoke(*static_cast<T*>(static_cast<void*>(&s.buf_)),
-                    std::forward<ArgTypes>(args)...);
-            }
-        };
-
-        template <class T>
-        struct large_handler : handler_base<large_handler<T>> {
-            template <class... Args>
-            static void create(storage& s, Args&&... args)
-            {
-                s.ptr_ = new T(std::forward<Args>(args)...);
-            }
-
-            static void destroy(storage& s) noexcept { delete static_cast<T*>(s.ptr_); }
-
-            static void move(storage& dst, storage& src) noexcept
-            {
-                dst.ptr_ = src.ptr_;
-            }
-
-            static R call(storage& s, ArgTypes... args)
-            {
-                return std::invoke(*static_cast<T*>(s.ptr_),
-                    std::forward<ArgTypes>(args)...);
-            }
-        };
-
-        template <class T>
-        using handler = std::conditional_t<is_small_object_v<T>, small_handler<T>,
-            large_handler<T>>;
-    };
-
-    template <class T>
-    struct is_in_place_type : std::false_type { };
-
-    template <class T>
-    struct is_in_place_type<std::in_place_type_t<T>> : std::true_type { };
-
-    template <class T>
-    inline constexpr auto is_in_place_type_v = is_in_place_type<T>::value;
-
-    template <class R, bool is_noexcept, class... ArgTypes>
-    class any_invocable_impl {
-        template <class T>
-        using handler =
-            typename any_detail::handler_traits<R, ArgTypes...>::template handler<T>;
-
-        using storage = any_detail::storage;
-        using action = any_detail::action;
-        using handle_func = void (*)(any_detail::action, any_detail::storage*,
-            any_detail::storage*);
-        using call_func = R (*)(any_detail::storage&, ArgTypes...);
-
-    public:
-        using result_type = R;
-
-        any_invocable_impl() noexcept = default;
-        any_invocable_impl(std::nullptr_t) noexcept { }
-        any_invocable_impl(any_invocable_impl&& rhs) noexcept
+template <class R, class... ArgTypes>
+struct handler_traits {
+    template <class Derived>
+    struct handler_base {
+        static void handle(action act, storage* current, storage* other = nullptr)
         {
+            switch (act) {
+            case (action::destroy):
+                Derived::destroy(*current);
+                break;
+            case (action::move):
+                Derived::move(*current, *other);
+                break;
+            }
+        }
+    };
+
+    template <class T>
+    struct small_handler : handler_base<small_handler<T>> {
+        template <class... Args>
+        static void create(storage& s, Args&&... args)
+        {
+            new (static_cast<void*>(&s.buf_)) T(std::forward<Args>(args)...);
+        }
+
+        static void destroy(storage& s) noexcept
+        {
+            T& value = *static_cast<T*>(static_cast<void*>(&s.buf_));
+            value.~T();
+        }
+
+        static void move(storage& dst, storage& src) noexcept
+        {
+            create(dst, std::move(*static_cast<T*>(static_cast<void*>(&src.buf_))));
+            destroy(src);
+        }
+
+        static R call(storage& s, ArgTypes... args)
+        {
+            return std::invoke(*static_cast<T*>(static_cast<void*>(&s.buf_)),
+                std::forward<ArgTypes>(args)...);
+        }
+    };
+
+    template <class T>
+    struct large_handler : handler_base<large_handler<T>> {
+        template <class... Args>
+        static void create(storage& s, Args&&... args)
+        {
+            s.ptr_ = new T(std::forward<Args>(args)...);
+        }
+
+        static void destroy(storage& s) noexcept { delete static_cast<T*>(s.ptr_); }
+
+        static void move(storage& dst, storage& src) noexcept
+        {
+            dst.ptr_ = src.ptr_;
+        }
+
+        static R call(storage& s, ArgTypes... args)
+        {
+            return std::invoke(*static_cast<T*>(s.ptr_),
+                std::forward<ArgTypes>(args)...);
+        }
+    };
+
+    template <class T>
+    using handler = std::conditional_t<is_small_object_v<T>, small_handler<T>,
+        large_handler<T>>;
+};
+
+template <class T>
+struct is_in_place_type : std::false_type { };
+
+template <class T>
+struct is_in_place_type<std::in_place_type_t<T>> : std::true_type { };
+
+template <class T>
+inline constexpr auto is_in_place_type_v = is_in_place_type<T>::value;
+
+template <class R, bool is_noexcept, class... ArgTypes>
+class any_invocable_impl {
+    template <class T>
+    using handler =
+        typename any_detail::handler_traits<R, ArgTypes...>::template handler<T>;
+
+    using storage = any_detail::storage;
+    using action = any_detail::action;
+    using handle_func = void (*)(any_detail::action, any_detail::storage*,
+        any_detail::storage*);
+    using call_func = R (*)(any_detail::storage&, ArgTypes...);
+
+public:
+    using result_type = R;
+
+    any_invocable_impl() noexcept = default;
+    any_invocable_impl(std::nullptr_t) noexcept { }
+    any_invocable_impl(any_invocable_impl&& rhs) noexcept
+    {
+        if (rhs.handle_) {
+            handle_ = rhs.handle_;
+            handle_(action::move, &storage_, &rhs.storage_);
+            call_ = rhs.call_;
+            rhs.handle_ = nullptr;
+        }
+    }
+
+    any_invocable_impl& operator=(any_invocable_impl&& rhs) noexcept
+    {
+        any_invocable_impl { std::move(rhs) }.swap(*this);
+        return *this;
+    }
+    any_invocable_impl& operator=(std::nullptr_t) noexcept
+    {
+        destroy();
+        return *this;
+    }
+
+    ~any_invocable_impl() { destroy(); }
+
+    void swap(any_invocable_impl& rhs) noexcept
+    {
+        if (handle_) {
             if (rhs.handle_) {
-                handle_ = rhs.handle_;
-                handle_(action::move, &storage_, &rhs.storage_);
-                call_ = rhs.call_;
-                rhs.handle_ = nullptr;
-            }
-        }
-
-        any_invocable_impl& operator=(any_invocable_impl&& rhs) noexcept
-        {
-            any_invocable_impl { std::move(rhs) }.swap(*this);
-            return *this;
-        }
-        any_invocable_impl& operator=(std::nullptr_t) noexcept
-        {
-            destroy();
-            return *this;
-        }
-
-        ~any_invocable_impl() { destroy(); }
-
-        void swap(any_invocable_impl& rhs) noexcept
-        {
-            if (handle_) {
-                if (rhs.handle_) {
-                    storage tmp;
-                    handle_(action::move, &tmp, &storage_);
-                    rhs.handle_(action::move, &storage_, &rhs.storage_);
-                    handle_(action::move, &rhs.storage_, &tmp);
-                    std::swap(handle_, rhs.handle_);
-                    std::swap(call_, rhs.call_);
-                } else {
-                    rhs.swap(*this);
-                }
-            } else if (rhs.handle_) {
+                storage tmp;
+                handle_(action::move, &tmp, &storage_);
                 rhs.handle_(action::move, &storage_, &rhs.storage_);
-                handle_ = rhs.handle_;
-                call_ = rhs.call_;
-                rhs.handle_ = nullptr;
+                handle_(action::move, &rhs.storage_, &tmp);
+                std::swap(handle_, rhs.handle_);
+                std::swap(call_, rhs.call_);
+            } else {
+                rhs.swap(*this);
             }
+        } else if (rhs.handle_) {
+            rhs.handle_(action::move, &storage_, &rhs.storage_);
+            handle_ = rhs.handle_;
+            call_ = rhs.call_;
+            rhs.handle_ = nullptr;
         }
+    }
 
-        explicit operator bool() const noexcept { return handle_ != nullptr; }
+    explicit operator bool() const noexcept { return handle_ != nullptr; }
 
-    protected:
-        template <class F, class... Args>
-        void create(Args&&... args)
-        {
-            using hdl = handler<F>;
-            hdl::create(storage_, std::forward<Args>(args)...);
-            handle_ = &hdl::handle;
-            call_ = &hdl::call;
+protected:
+    template <class F, class... Args>
+    void create(Args&&... args)
+    {
+        using hdl = handler<F>;
+        hdl::create(storage_, std::forward<Args>(args)...);
+        handle_ = &hdl::handle;
+        call_ = &hdl::call;
+    }
+
+    void destroy() noexcept
+    {
+        if (handle_) {
+            handle_(action::destroy, &storage_, nullptr);
+            handle_ = nullptr;
         }
+    }
 
-        void destroy() noexcept
-        {
-            if (handle_) {
-                handle_(action::destroy, &storage_, nullptr);
-                handle_ = nullptr;
-            }
-        }
+    R call(ArgTypes... args) noexcept(is_noexcept)
+    {
+        return call_(storage_, std::forward<ArgTypes>(args)...);
+    }
 
-        R call(ArgTypes... args) noexcept(is_noexcept)
-        {
-            return call_(storage_, std::forward<ArgTypes>(args)...);
-        }
+    friend bool operator==(const any_invocable_impl& f, std::nullptr_t) noexcept
+    {
+        return !f;
+    }
+    friend bool operator==(std::nullptr_t, const any_invocable_impl& f) noexcept
+    {
+        return !f;
+    }
+    friend bool operator!=(const any_invocable_impl& f, std::nullptr_t) noexcept
+    {
+        return static_cast<bool>(f);
+    }
+    friend bool operator!=(std::nullptr_t, const any_invocable_impl& f) noexcept
+    {
+        return static_cast<bool>(f);
+    }
 
-        friend bool operator==(const any_invocable_impl& f, std::nullptr_t) noexcept
-        {
-            return !f;
-        }
-        friend bool operator==(std::nullptr_t, const any_invocable_impl& f) noexcept
-        {
-            return !f;
-        }
-        friend bool operator!=(const any_invocable_impl& f, std::nullptr_t) noexcept
-        {
-            return static_cast<bool>(f);
-        }
-        friend bool operator!=(std::nullptr_t, const any_invocable_impl& f) noexcept
-        {
-            return static_cast<bool>(f);
-        }
+    friend void swap(any_invocable_impl& lhs, any_invocable_impl& rhs) noexcept
+    {
+        lhs.swap(rhs);
+    }
 
-        friend void swap(any_invocable_impl& lhs, any_invocable_impl& rhs) noexcept
-        {
-            lhs.swap(rhs);
-        }
+private:
+    storage storage_;
+    handle_func handle_ = nullptr;
+    call_func call_;
+};
 
-    private:
-        storage storage_;
-        handle_func handle_ = nullptr;
-        call_func call_;
-    };
+template <class T>
+using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
 
-    template <class T>
-    using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
-
-    template <class AI, class F, bool noex, class R, class FCall, class... ArgTypes>
-    using can_convert = std::conjunction<
-        std::negation<std::is_same<remove_cvref_t<F>, AI>>,
-        std::negation<any_detail::is_in_place_type<remove_cvref_t<F>>>,
-        std::is_invocable_r<R, FCall, ArgTypes...>,
-        std::bool_constant<(!noex || std::is_nothrow_invocable_r_v<R, FCall, ArgTypes...>)>,
-        std::is_constructible<std::decay_t<F>, F>>;
+template <class AI, class F, bool noex, class R, class FCall, class... ArgTypes>
+using can_convert = std::conjunction<
+    std::negation<std::is_same<remove_cvref_t<F>, AI>>,
+    std::negation<any_detail::is_in_place_type<remove_cvref_t<F>>>,
+    std::is_invocable_r<R, FCall, ArgTypes...>,
+    std::bool_constant<(!noex || std::is_nothrow_invocable_r_v<R, FCall, ArgTypes...>)>,
+    std::is_constructible<std::decay_t<F>, F>>;
 
 } // namespace any_detail
 
@@ -388,6 +393,8 @@ __OFATS_ANY_INVOCABLE(const, &&, true, const&&) // 121
 template <class T>
 using MoveOnlyFunction = ofats::any_invocable<T>;
 #else
-template<typename T>
+template <typename T>
 using MoveOnlyFunction = uWS::MoveOnlyFunction<T>;
+
 #endif // _ANY_INVOKABLE_H_
+#endif
